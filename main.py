817c13709c175d506f25a1885a88f809f7623ec3
soojingyuan <119158537+soojingyuan@users.noreply.github.com>
2023-01-12 17:28:37 +0800

    Add initial CV website file

---
+++ main.py
@@ -0,0 +1,22 @@
+# html is the structure of the website
+# css is the styling
+# JS is the behavior
+
+# what is HTML
+# website is to interpret the code
+# foundation is the HTML
+# HTML = HyperText Markup Text
+
+
+# <h1> header = word size
+# <br> = break
+# <hr> = line
+# <hr size="3>
+# attribute
+# <DOCTYPE is html 5
+# <html opening and closing tag
+# head and body
+# head is the brain function
+# body is the page
+# <p> <em> <strong> <ul> = an un-ordered list <ol> order
+# deprecated
